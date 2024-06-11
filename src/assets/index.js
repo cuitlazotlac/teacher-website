1d0b7a9c9b7fb210eefdc5379e4ee3135bd4a734
hveluxor <cuitlazotlac@gmail.com>
2024-06-10 23:34:21 -0400

    new changes to the UI

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import brainwave from "./brainwave.svg";
+import brainwave from "./madamelise.svg";
 import check from "./check.svg";
 import brainwaveSymbol from "./brainwave-symbol.svg";
 import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
@@ -19,7 +19,7 @@ import disc02 from "./disc-02.svg";
 import chromecast from "./chrome-cast.svg";
 import sliders04 from "./sliders-04.svg";
 import loading from "./loading.png";
-import background from "./background.jpg";
+import background from "./background-new.png";
 
 import curve from "./hero/curve.png";
 import robot from "./hero/robot.jpg";
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,20 +22,20 @@ const Hero = () => {
     >
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
-          <h1 className="h1 mb-6">
+          <h1 className="mb-6 h1">
             Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
-            <span className="inline-block relative">
+            <span className="relative inline-block">
               Brainwave{" "}
               <img
                 src={curve}
-                className="absolute top-full left-0 w-full xl:-mt-2"
+                className="absolute left-0 w-full top-full xl:-mt-2"
                 width={624}
                 height={28}
                 alt="Curve"
               />
             </span>
           </h1>
-          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+          <p className="max-w-3xl mx-auto mb-6 body-1 text-n-2 lg:mb-8">
             Unleash the power of AI within Brainwave. Upgrade your productivity
             with Brainwave, the open AI chat app.
           </p>
@@ -93,7 +93,7 @@ const Hero = () => {
           <BackgroundCircles />
         </div>
 
-        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
+        <CompanyLogos className="relative z-10 hidden mt-20 lg:block" />
       </div>
 
       <BottomLine />
